a6a8cd2f4d3fa872eb1cdce8f8b2d259e012e31f
Adam Andrus and Remy <AdamAndrus11@gmail.com>
2016-09-14 12:09:10 -0700

    Initial commit

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -76,6 +76,6 @@ class SightingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sighting_params
-      params.require(:sighting).permit(:date, :time, :latitude, :longitude, :animal_id)
+      params.require(:sighting).permit(:date, :time, :latitude, :longitude, :region, :animal_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913223939) do
+ActiveRecord::Schema.define(version: 20160914180413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160913223939) do
     t.integer  "animal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "region"
   end
 
   add_index "sightings", ["animal_id"], name: "index_sightings_on_animal_id", using: :btree
